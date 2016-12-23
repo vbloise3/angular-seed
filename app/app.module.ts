5bc0fc7a11f3dc7d8b89851663357ad9a81c3f2a
Vince Bloise <vbloise3@verizon.net>
2016-12-23 15:04:42 -0500

    created new version of base project

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import  AppComponent   from './app.component';
 
+//add my own component
 import  YoMoFoComponent  from './yo-mo-fo';
 
 @NgModule({
