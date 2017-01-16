03cb9f7633c049e25e535af7a41ef1c4443b0515
Vince Bloise <vbloise3@verizon.net>
2017-01-15 22:25:37 -0500

    Converted to simple AWS node app

--- app/main.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by vincebloise on 6/11/16.
- */
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule }  from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
